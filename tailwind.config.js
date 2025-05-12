73fb966de4042b74149bd4a2824c66537c60cd29
Rahul <rparjapati123456@gmail.com>
2025-05-12 00:16:58 -0700

    tailwind setup and external font added

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        lightBlue: "#404EED",
+      },
+      fontFamily: {
+        ggsans: ["ggsans-normal"],
+        ggsans: ['GG Sans', 'sans-serif'], 
+      },
+      spacing: {
+        30: "120px",
+      },
+    },
+  },
+  plugins: [],
+};
